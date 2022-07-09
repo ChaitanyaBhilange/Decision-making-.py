8a1c1a81fb7e458af420c43adb9adabe8bd827aa
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-08 20:27:47 -0700

    Add files via upload

---
+++ decision making ex.py
@@ -0,0 +1,13 @@
+#if statement
+c=input("Enter any Chatacter=")
+if c=='a'or c=='e' or c=='i' or c=='o' or c=='u': 
+    print("Vowel")
+else:
+    print("Non Vowel")
+
+    
+'''if c==('A' or c=='E' or c=='I' or c=='O' or c=='U'):
+    print("Vowel")
+else:
+    print("Non Vowel")
+'''
